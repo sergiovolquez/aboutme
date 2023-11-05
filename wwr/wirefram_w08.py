f222205a02de73a80471a8f4125cb012fea500cd
sergiovolquez <sergiovolquez12@gmail.com>
2023-11-04 22:01:58 -0700

    wirefram_w08

---
+++ wwr/wirefram_w08.py
@@ -0,0 +1,42 @@
+---------------------------------------------------
+|               Header (Consistent)               |
+---------------------------------------------------
+
+Page 2 - Rafting Adventure
+
+---------------------------------------------------
+|               Navigation (Consistent)           |
+| Home | Page 2 | About | Contact                 |
+---------------------------------------------------
+
+---------------------------------------------------
+|                                                |
+|               [Your Image]                      |
+|                                                |
+---------------------------------------------------
+
+---------------------------------------------------
+|                 Page Content                    |
+|                                                |
+|                                                |
+|   Welcome to our Rafting Adventure Page!        |
+|                                                |
+|   Join us for an exciting river adventure.      |
+|   Experience the thrill of white-water rafting  |
+|   in the beautiful natural surroundings.        |
+|                                                |
+|   Activities include:                          |
+|   - White-water rafting                        |
+|   - Kayaking                                   |
+|   - Camping by the river                      |
+|                                                |
+|   Book your adventure now!                     |
+|                                                |
+|   Contact us for more information.              |
+|                                                |
+---------------------------------------------------
+
+---------------------------------------------------
+|               Footer (Consistent)               |
+| Copyright © 2023 | Privacy Policy | Sitemap    |
+---------------------------------------------------
